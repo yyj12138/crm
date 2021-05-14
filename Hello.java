ccf4ed938ba38a65eeccfce4b047ae792efdb500
mldn <mdlnqa@163.com>
2021-05-14 15:15:38 +0800

    Test FF Merge

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 package cn.mldn ;
 public class Hello {
 	public static void main(String args[]) {
-		System.out.println("www.mldn.cn") ;
+		Runtime run=Runtime.getRuntime;
+		run.gc();
+		System.out.println("Hello GIT");
+		System.out.println("good job");
+		System.out.println("nihao");
 	}
 }
